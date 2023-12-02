15132fbb19a8a8cb12b83acc222d7439b5e14655
mahadi025 <mahadikarimmunif@gmail.com>
2023-12-03 00:04:01 +0600

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿// using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    // [Key] //To use as a primary key but not needed here because Id will be set as a primary key automatically
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
