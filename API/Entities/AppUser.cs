ea0d78928d64f805983a6a3f0c88113a58103be5
mahadi025 <mahadikarimmunif@gmail.com>
2023-12-04 11:48:26 +0600

    Added Password to the user

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,4 +7,6 @@ public class AppUser
     // [Key] //To use as a primary key but not needed here because Id will be set as a primary key automatically
     public int Id { get; set; }
     public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
