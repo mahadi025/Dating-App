fbfb45dde8806fa90d3a557b29bb1a2e8611f558
mahadi025 <mahadikarimmunif@gmail.com>
2023-12-05 11:37:11 +0600

    Removed unnecessary imports

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,3 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
