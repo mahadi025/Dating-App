87500ed64764a0e41f3340f609d52a16801185d9
mahadi025 <mahadikarimmunif@gmail.com>
2023-12-25 20:28:57 +0600

    implemented Pagination to the api

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -43,11 +44,13 @@ public class UserRepository : IUserRepository
         _context.Entry(user).State = EntityState.Modified;
     }
 
-    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
-        return await _context.Users
+        var query = _context.Users
         .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-        .ToArrayAsync();
+        .AsNoTracking();
+
+        return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
     }
 
     public async Task<MemberDto> GetMemberAsync(string username)
---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using API.Helpers;
+
+namespace API.Extensions;
+
+public static class HttpExtensions
+{
+    public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
+    {
+        var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        response.Headers.Append("Pagination", JsonSerializer.Serialize(header, jsonOptions)); //used Append instead of  Add
+        response.Headers.Append("Access-Control-Expose-Headers", "Pagination");//used Append instead of  Add
+    }
+}
