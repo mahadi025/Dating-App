61d497a744e8da2ecd38a54150796e1fad34e0fa
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-01 21:13:27 +0600

    added member-messages

---
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from 'src/app/_models/message';
+
+@Component({
+  selector: 'app-member-messages',
+  standalone: true,
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css'],
+  imports: [CommonModule, TimeagoModule]
+})
+export class MemberMessagesComponent implements OnInit {
+
+  @Input() username?: string;
+  @Input() messages: Message[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+
+}
