220d54936016a610f9b8c0f337ddfe74633902a9
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-02 18:35:55 +0600

    updated the validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -43,7 +43,7 @@ export class RegisterComponent implements OnInit {
 
   matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
+      return control.value === control.parent?.get(matchTo)?.value ? null : { isMatching: true }
     }
   }
 
