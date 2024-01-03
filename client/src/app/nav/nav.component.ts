6157759c653c2e3a4232cc76cc4b77c6515bff72
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-04 00:15:29 +0600

    clear the login info after logout

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,6 +23,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: () => {
         this.router.navigateByUrl('/members');
+        this.model = {};
       },
     })
   }
